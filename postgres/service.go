4adaecca1f90f5fa8022271342b9a388efa10b15
Joe Timmer <profsmallpine@gmail.com>
2021-03-24 13:33:54 -0600

    Uses session to conduct count

--- postgres/service.go
+++ postgres/service.go
@@ -116,7 +116,7 @@ func (service *DatabaseServiceImpl) PagedByQueryFromSession(models interface{}, 
 
 	// Conduct unlimited count query to calculate totals
 	var totalRecords int64
-	if err := service.db.Where("deleted_at IS NULL").Find(models).Count(&totalRecords).Error; err != nil {
+	if err := session.Find(models).Count(&totalRecords).Error; err != nil {
 		return pd, err
 	}
 
