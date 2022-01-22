3caf3a59e88fb414de5ee12a34e0821b5de85394
David Ketch <david@xyplanningnetwork.com>
2022-01-22 07:47:50 -0600

    http/ctx: KeyRing -> Keyring

--- http/ctx/keyring.go
+++ http/ctx/keyring.go
@@ -12,30 +12,30 @@ func (k Key) String() string {
 
 const _ Key = ""
 
-// Something KeyRingable because it stores arbitrary keys, accessible by a string name,
+// Something Keyringable because it stores arbitrary keys, accessible by a string name,
 // and makes it convenient to grab a CurrentUserKey or SessionKey.
-type KeyRingable interface {
+type Keyringable interface {
 	CurrentUserKey() CtxKeyable
 	Key(name string) CtxKeyable
 	SessionKey() CtxKeyable
 	keys() map[string]CtxKeyable
 }
 
-// KeyRing stores CtxKeyables and cannot be mutated outside of a constructor.
-type KeyRing struct {
+// Keyring stores CtxKeyables and cannot be mutated outside of a constructor.
+type Keyring struct {
 	session     string
 	currentUser string
 	internal    map[string]CtxKeyable
 }
 
-// NewKeyRing constructs a KeyRing from the given CtxKeyables.
-// NewKeyRing requires keys to be retrieved through CurrentUserKey() and SessionKey(), respectively.
-// NewKeyRing accepts an arbitrary number of other CtxKeyables, accessible through the Key method.
-func NewKeyRing(sessionKey, currentUserKey CtxKeyable, additional ...CtxKeyable) KeyRingable {
+// NewKeyring constructs a Keyring from the given CtxKeyables.
+// NewKeyring requires keys to be retrieved through CurrentUserKey() and SessionKey(), respectively.
+// NewKeyring accepts an arbitrary number of other CtxKeyables, accessible through the Key method.
+func NewKeyring(sessionKey, currentUserKey CtxKeyable, additional ...CtxKeyable) Keyringable {
 	if sessionKey == nil || currentUserKey == nil {
 		return nil
 	}
-	kr := &KeyRing{
+	kr := &Keyring{
 		session:     sessionKey.Key(),
 		currentUser: currentUserKey.Key(),
 		internal: map[string]CtxKeyable{
@@ -54,30 +54,30 @@ func NewKeyRing(sessionKey, currentUserKey CtxKeyable, additional ...CtxKeyable)
 	return kr
 }
 
-// CurrentUserKey returns the key set in the currentUserKey parameter of NewKeyRing or nil.
-func (kr *KeyRing) CurrentUserKey() CtxKeyable {
+// CurrentUserKey returns the key set in the currentUserKey parameter of NewKeyring or nil.
+func (kr *Keyring) CurrentUserKey() CtxKeyable {
 	return kr.internal[kr.currentUser]
 }
 
 // Key returns the key by name (i.e., CtxKeyable.Key()) or nil.
-func (kr *KeyRing) Key(name string) CtxKeyable {
+func (kr *Keyring) Key(name string) CtxKeyable {
 	return kr.internal[name]
 }
 
-// SessionKey returns the key set in the sessionKey parameter of NewKeyRing or nil.
-func (kr *KeyRing) SessionKey() CtxKeyable {
+// SessionKey returns the key set in the sessionKey parameter of NewKeyring or nil.
+func (kr *Keyring) SessionKey() CtxKeyable {
 	return kr.internal[kr.session]
 }
 
 // keys exposes the internal map of CtxKeyables.
-func (kr *KeyRing) keys() map[string]CtxKeyable { return kr.internal }
+func (kr *Keyring) keys() map[string]CtxKeyable { return kr.internal }
 
-// WithKeyRing constructs a new KeyRingable from the parent
-// and adds additional CtxKeyables to the new KeyRingable.
-func WithKeyRing(parent KeyRingable, additional ...CtxKeyable) KeyRingable {
+// WithKeyring constructs a new Keyringable from the parent
+// and adds additional CtxKeyables to the new Keyringable.
+func WithKeyring(parent Keyringable, additional ...CtxKeyable) Keyringable {
 	sk := parent.SessionKey()
 	ck := parent.CurrentUserKey()
-	kr := &KeyRing{
+	kr := &Keyring{
 		session:     sk.Key(),
 		currentUser: ck.Key(),
 		internal:    make(map[string]CtxKeyable),
