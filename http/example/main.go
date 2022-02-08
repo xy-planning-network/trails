5267fe777d4d145d4fcdeccceacdff17b5df4159
David Ketch <david@xyplanningnetwork.com>
2022-02-08 06:10:59 -0600

    wip

--- http/example/main.go
+++ http/example/main.go
@@ -8,21 +8,21 @@ package main
 import (
 	"embed"
 	"errors"
+	"fmt"
+	html "html/template"
 	"net/http"
-	"net/url"
-	"os"
 	"time"
 
 	"github.com/xy-planning-network/trails/http/keyring"
 	"github.com/xy-planning-network/trails/http/middleware"
 	. "github.com/xy-planning-network/trails/http/resp"
 	"github.com/xy-planning-network/trails/http/router"
-	"github.com/xy-planning-network/trails/http/session"
 	"github.com/xy-planning-network/trails/http/template"
 	"github.com/xy-planning-network/trails/logger"
+	"github.com/xy-planning-network/trails/ranger"
 )
 
-//go:embed *.tmpl nested/*.tmpl
+//go:embed *.tmpl nested/*.tmpl tmpl/layout/*.tmpl
 var files embed.FS
 
 type ctxKey string
@@ -55,8 +55,12 @@ type Handler struct {
 	Ring keyring.Keyringable
 }
 
+type RangerHandler struct {
+	*ranger.Ranger
+}
+
 // root is a fully-formed use of Responder.
-func (h *Handler) root(w http.ResponseWriter, r *http.Request) {
+func (h *RangerHandler) root(w http.ResponseWriter, r *http.Request) {
 	data := map[string]any{
 		"sick": "such data",
 		"wow":  "so data",
@@ -72,8 +76,8 @@ func (h *Handler) root(w http.ResponseWriter, r *http.Request) {
 // in this example we show how values provided in one response do not bleed into another.
 // to test this out, throw in different values into a query param: ?name=
 // then, remove it from your request to see the name resets.
-func (h *Handler) withCurrentUser(w http.ResponseWriter, r *http.Request) {
-	_, ok := r.Context().Value(h.Ring.CurrentUserKey()).(user)
+func (h *RangerHandler) withCurrentUser(w http.ResponseWriter, r *http.Request) {
+	_, ok := r.Context().Value(h.Ranger.Keyring.CurrentUserKey()).(middleware.User)
 	if !ok {
 		err := errors.New("no user")
 		h.Redirect(w, r, Err(err), ToRoot())
@@ -87,86 +91,50 @@ func (h *Handler) withCurrentUser(w http.ResponseWriter, r *http.Request) {
 
 // incorrect shows how a template not actually referred to by the base does
 // not break our ability to call Html.
-func (h *Handler) incorrect(w http.ResponseWriter, r *http.Request) {
+func (h *RangerHandler) incorrect(w http.ResponseWriter, r *http.Request) {
 	if err := h.Html(w, r, Unauthed(), Tmpls(other, nav, content)); err != nil {
 		h.Err(w, r, err)
 	}
 }
 
 // broken cannot render because no authed template was set on the Responder.
-func (h *Handler) broken(w http.ResponseWriter, r *http.Request) {
+func (h *RangerHandler) broken(w http.ResponseWriter, r *http.Request) {
 	if err := h.Html(w, r, Authed()); err != nil {
 		h.Err(w, r, err)
 	}
 }
 
+type lo string
+
+func (l lo) Debug(_ string, _ *logger.LogContext) { fmt.Println(l) }
+func (l lo) Info(_ string, _ *logger.LogContext)  { fmt.Println(l) }
+func (l lo) Warn(_ string, _ *logger.LogContext)  { fmt.Println(l) }
+func (l lo) Error(_ string, _ *logger.LogContext) { fmt.Println(l) }
+func (l lo) Fatal(_ string, _ *logger.LogContext) { fmt.Println(l) }
+func (lo) LogLevel() logger.LogLevel              { return 99999999 }
+
+type poopoo string
+
+func (poopoo) AddFn(_ string, _ interface{}) {}
+func (p poopoo) Parse(_ ...string) (*html.Template, error) {
+	return html.Must(html.New("unauthenticated_base.tmpl").Parse(string(p))), nil
+}
+
+var (
+	_ logger.Logger   = lo("hey")
+	_ template.Parser = poopoo("hey")
+)
+
 func main() {
-	env := "EXAMPLE"
-	log := logger.New()
-	u, err := url.ParseRequestURI("localhost:8081")
+	p := ranger.DefaultParser(files, template.WithFn("hammer", itsHammerTime))
+	rng, err := ranger.NewRanger(p)
 	if err != nil {
-		log.Fatal(err.Error(), nil)
-		os.Exit(1)
+		fmt.Println(err)
+		return
 	}
 
-	// Setup new keyring.Keyring
-	ring := keyring.NewKeyring(sessionKey, userKey)
-
-	// Setup new template.Parser
-	//
-	// notably, many of the functions passed in are closures
-	// we thereby make available to all handlers values that are like constants/globals
-	p := template.NewParser(
-		files,
-		template.WithFn("hammer", itsHammerTime),
-		template.WithFn(template.Env(env)),
-		template.WithFn(template.RootUrl(u)),
-	)
-
-	// Setup new resp.Responder
-	d := NewResponder(
-		WithSessionKey(ring.SessionKey()),
-		WithUserSessionKey(ring.CurrentUserKey()),
-		WithRootUrl("/"),
-		WithParser(p),
-		WithAuthTemplate(auth),
-		WithUnauthTemplate(base),
-	)
-
-	// Setup session store
-	sessionstore, err := session.NewStoreService(env, "f0f42f970982be947b6536df8a0d2489d7f06d15e6d36cfbfae7ac16ccbe7cc59780b3fac338e4e0d3c5968b1fa24a884932547614ce16fdb1a18ba089f44af0", "0bc7730e33bb9ef91197239cb3a44fb43a4f3fac7b36e36afd4f2b2d8e920b65")
-	if err != nil {
-		log.Fatal(err.Error(), nil)
-		os.Exit(1)
-	}
-
-	// Setup user store
-	userstore := make(users, 0)
-
-	// Setup visitor tracker
-	vs := middleware.NewVisitors()
-
-	// Setup handler manager
-	h := &Handler{Responder: d, Ring: ring}
-
-	// Setup router
-	r := router.NewRouter(env)
-	r.HandleNotFound(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(404)
-		w.Write([]byte("Oops no " + r.URL.Path))
-	})
-
-	// Include the following default middleware stack, executing on every HTTP request.
-	r.OnEveryRequest(
-		middleware.RateLimit(vs),
-		middleware.LogRequest(log),
-		middleware.InjectIPAddress(),
-		middleware.InjectSession(sessionstore, ring.SessionKey()),
-		registerUser(ring.SessionKey(), &userstore),
-		middleware.CurrentUser(d, &userstore, ring.SessionKey(), ring.CurrentUserKey()),
-	)
-
-	r.HandleRoutes(
+	h := &RangerHandler{Ranger: rng}
+	rng.Router.HandleRoutes(
 		[]router.Route{
 			{Path: "/broken", Method: http.MethodGet, Handler: h.broken},
 			{Path: "/incorrect", Method: http.MethodGet, Handler: h.incorrect},
@@ -174,7 +142,87 @@ func main() {
 			{Path: "/", Method: http.MethodGet, Handler: h.root},
 		},
 	)
+	rng.Router.HandleNotFound(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("not found")) }))
 
-	// Run the web server
-	http.ListenAndServe(u.String(), r)
+	if err := rng.Guide(); err != nil {
+		fmt.Println(err)
+		return
+	}
+	/*
+		log := logger.NewLogger()
+		u, err := url.ParseRequestURI("localhost:8081")
+		if err != nil {
+			log.Fatal(err.Error(), nil)
+			os.Exit(1)
+		}
+
+		// Setup new keyring.Keyring
+		ring := keyring.NewKeyring(sessionKey, userKey)
+
+		// Setup new template.Parser
+		//
+		// notably, many of the functions passed in are closures
+		// we thereby make available to all handlers values that are like constants/globals
+		p := template.NewParser(
+			files,
+			template.WithFn("hammer", itsHammerTime),
+			template.WithFn(template.Env(env)),
+			template.WithFn(template.RootUrl(u)),
+		)
+
+		// Setup new resp.Responder
+		d := NewResponder(
+			WithSessionKey(ring.SessionKey()),
+			WithUserSessionKey(ring.CurrentUserKey()),
+			WithRootUrl("/"),
+			WithParser(p),
+			WithAuthTemplate(auth),
+			WithUnauthTemplate(base),
+		)
+
+		// Setup session store
+		sessionstore, err := session.NewStoreService(env, "f0f42f970982be947b6536df8a0d2489d7f06d15e6d36cfbfae7ac16ccbe7cc59780b3fac338e4e0d3c5968b1fa24a884932547614ce16fdb1a18ba089f44af0", "0bc7730e33bb9ef91197239cb3a44fb43a4f3fac7b36e36afd4f2b2d8e920b65")
+		if err != nil {
+			log.Fatal(err.Error(), nil)
+			os.Exit(1)
+		}
+
+		// Setup user store
+		userstore := make(users, 0)
+
+		// Setup visitor tracker
+		vs := middleware.NewVisitors()
+
+		// Setup handler manager
+		h := &Handler{Responder: d, Ring: ring}
+
+		// Setup router
+		r := router.NewRouter(env)
+		r.HandleNotFound(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(404)
+			w.Write([]byte("Oops no " + r.URL.Path))
+		})
+
+		// Include the following default middleware stack, executing on every HTTP request.
+		r.OnEveryRequest(
+			middleware.RateLimit(vs),
+			middleware.LogRequest(log),
+			middleware.InjectIPAddress(),
+			middleware.InjectSession(sessionstore, ring.SessionKey()),
+			registerUser(ring.SessionKey(), &userstore),
+			middleware.CurrentUser(d, &userstore, ring.SessionKey(), ring.CurrentUserKey()),
+		)
+
+		r.HandleRoutes(
+			[]router.Route{
+				{Path: "/broken", Method: http.MethodGet, Handler: h.broken},
+				{Path: "/incorrect", Method: http.MethodGet, Handler: h.incorrect},
+				{Path: "/with-user", Method: http.MethodGet, Handler: h.withCurrentUser},
+				{Path: "/", Method: http.MethodGet, Handler: h.root},
+			},
+		)
+
+		// Run the web server
+		http.ListenAndServe(u.String(), r)
+	*/
 }
