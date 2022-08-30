2d241f4eb0a6d2828970b9702a9baa21ef695207
David Ketch <david@xyplanningnetwork.com>
2022-08-30 16:05:35 -0500

    http/example: updates unit tests for new default port

--- http/example/start-here/main_test.go
+++ http/example/start-here/main_test.go
@@ -4,6 +4,8 @@ import (
 	"net/http"
 	"os"
 	"testing"
+
+	"github.com/xy-planning-network/trails/ranger"
 )
 
 func Test(t *testing.T) {
@@ -26,7 +28,7 @@ func Test(t *testing.T) {
 		{"authed-200", "/authed", http.StatusOK},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-			actual, err := http.Get("http://localhost:8080" + tc.input)
+			actual, err := http.Get("http://" + ranger.DefaultHost + ranger.DefaultPort + tc.input)
 			if err != nil {
 				t.Fatal(err)
 			}
