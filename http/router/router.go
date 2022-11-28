85dc7aca80b79707474eb40b731f162d8de9b276
David Ketch <david@xyplanningnetwork.com>
2022-11-28 06:44:55 -0600

    http/router: docs update leveraging Go 1.19 features

--- http/router/router.go
+++ http/router/router.go
@@ -14,8 +14,8 @@ const (
 	clientDistPath   = "client/dist/"
 )
 
-// A Route maps a path and HTTP method to an http.HandlerFunc.
-// Additional middleware.Adapters can be called when a server handles
+// A Route maps a path and HTTP method to an [http.HandlerFunc].
+// Additional [middleware.Adapter] can be called when a server handles
 // a request matching the Route.
 type Route struct {
 	Path        string
@@ -24,15 +24,15 @@ type Route struct {
 	Middlewares []middleware.Adapter
 }
 
-// A Router handles many Routes, directing HTTP requests to the appropriate endpoint.
+// A Router handles many [Route], directing HTTP requests to the appropriate endpoint.
 type Router interface {
 	// AuthedRoutes registers the set of Routes as those requiring authentication.
 	AuthedRoutes(key keyring.Keyable, loginUrl string, logoffUrl string, routes []Route, middlewares ...middleware.Adapter)
 
-	// Handle applies the Route to the Router
+	// Handle applies the [Route] to the Router
 	Handle(route Route)
 
-	// HandleNotFound sets the provided http.HandlerFunc as the default function
+	// HandleNotFound sets the provided [http.HandlerFunc] as the default function
 	// for when no other registered Route is matched.
 	HandleNotFound(handler http.HandlerFunc)
 
@@ -42,7 +42,7 @@ type Router interface {
 
 	// OnEveryRequest sets the middleware stack to be applied before every request
 	//
-	// Other methods applying a set of middleware.Adapters will always apply theirs
+	// Other methods applying a set of [middleware.Adapter] will always apply theirs
 	// after the set defined by OnEveryRequest.
 	OnEveryRequest(middlewares ...middleware.Adapter)
 
@@ -57,7 +57,7 @@ type Router interface {
 
 // The DefaultRouter handles HTTP requests to any Routes it is configured with.
 //
-// DefaultRouter applies the middleware.ReportPanic handler to all registered routes.
+// DefaultRouter applies the [middleware.ReportPanic] handler to all registered routes.
 //
 // DefaultRouter routes requests for assets to their location in a standard trails app layout.
 // DefaultRouter applies a "Cache-Control" header to responses for assets.
@@ -68,6 +68,14 @@ type DefaultRouter struct {
 }
 
 // AuthedRoutes registers the set of Routes as those requiring authentication.
+// AuthedRoutes applies the given middlewares before performing that check,
+// using middleware.RequireAuthed.
+//
+// middleware.RequireAuthed requires loginUrl and logoffUrl to appropriately
+// redirect applicable requests.
+// middlweare.RequireAuthed uses key to check whether a user is authenticated or not.
+//
+// key ought to be the one returned by your keyring.Keyringable.CurrentUserKey.
 func (r *DefaultRouter) AuthedRoutes(
 	key keyring.Keyable,
 	loginUrl string,
@@ -78,9 +86,9 @@ func (r *DefaultRouter) AuthedRoutes(
 	r.HandleRoutes(routes, append(middlewares, middleware.RequireAuthed(key, loginUrl, logoffUrl))...)
 }
 
-// NewRouter constructs an implementation of Router using DefaultRouter for the given environment.
+// NewRouter constructs an implementation of [Router] using [DefaultRouter] for the given environment.
 //
-// TODO(dlk): use provided fs.FS and http.FS instead of http.FileServer.
+// TODO(dlk): use provided [fs.FS] and [http.FS] instead of [http.FileServer].
 func NewRouter(env string) Router {
 	r := mux.NewRouter()
 
@@ -99,19 +107,20 @@ func NewRouter(env string) Router {
 	return &DefaultRouter{Env: env, Router: r}
 }
 
-// Handle applies the Route to the Router, wrapping the Handler in middleware.ReportPanic.
+// Handle applies the [Route] to the [Router].
 func (r *DefaultRouter) Handle(route Route) {
 	r.HandleRoutes([]Route{route})
 }
 
-// HandleNotFound sets the provided http.HandlerFunc as the default function
+// HandleNotFound sets the provided [http.HandlerFunc] as the default function
 // for when no other registered Route is matched.
 func (r *DefaultRouter) HandleNotFound(handler http.HandlerFunc) {
 	r.Router.NotFoundHandler = middleware.ReportPanic(r.Env)(handler)
 }
 
-// HandleRoutes registers the set of Routes on the Router and includes all the middleware.Adapters on each Route.
-// Any middleware.Adapters already assigned to a Route are appended to middlewares,
+// HandleRoutes registers the set of Routes on the Router
+// and includes all the [middleware.Adapter] on each Route.
+// Any [middleware.Adapter] already assigned to a Route is appended to middlewares,
 // so are called after the default set.
 func (r *DefaultRouter) HandleRoutes(routes []Route, middlewares ...middleware.Adapter) {
 	for _, route := range routes {
@@ -130,7 +139,7 @@ func (r *DefaultRouter) HandleRoutes(routes []Route, middlewares ...middleware.A
 }
 
 // OnEveryRequest appends the middlewares to the existing stack
-// that the DefaultRouter will apply to every request.
+// that the [*DefaultRouter] will apply to every request.
 func (r *DefaultRouter) OnEveryRequest(middlewares ...middleware.Adapter) {
 	r.everyReqStack = append(r.everyReqStack, middlewares...)
 }
@@ -140,7 +149,7 @@ func (r *DefaultRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	r.Router.ServeHTTP(w, req)
 }
 
-// Subrouter constructs a Router that handles requests to endpoints matching the prefix.
+// Subrouter constructs a [Router] that handles requests to endpoints matching the prefix.
 //
 // e.g., r.Subrouter("/api/v1") handles requests to endpoints like /api/v1/users
 func (r *DefaultRouter) Subrouter(prefix string) Router {
@@ -151,7 +160,11 @@ func (r *DefaultRouter) Subrouter(prefix string) Router {
 	}
 }
 
-// UnauthedRoutes registers the set of Routes as those requiring unauthenticated users.
+// UnauthedRoutes registers routes as those requiring unauthenticated users.
+// It applies the given middlewares before performing that check.
+// It uses key to check whether a user is authenticated or not.
+//
+// key ought to be the one returned by your keyring.Keyringable.CurrentUserKey.
 func (r *DefaultRouter) UnauthedRoutes(key keyring.Keyable, routes []Route, middlewares ...middleware.Adapter) {
 	r.HandleRoutes(routes, append(middlewares, middleware.RequireUnauthed(key))...)
 }
