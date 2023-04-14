527552a250cde7637d247d0378ae69dc08ec17f6
David Ketch <david@xyplanningnetwork.com>
2023-04-14 10:00:16 -0500

    toolbox_test: unit tests

--- toolbox_test.go
+++ toolbox_test.go
@@ -8,13 +8,53 @@ import (
 )
 
 func TestToolboxFilter(t *testing.T) {
-	// TODO
-	toolbox := make(trails.Toolbox, 1)
-	require.Len(t, toolbox.Filter(), 0)
+	for _, tc := range []struct {
+		name   string
+		input  trails.Toolbox
+		output trails.Toolbox
+	}{
+		{"Nil", nil, make(trails.Toolbox, 0)},
+		{"Zero", make(trails.Toolbox, 0), make(trails.Toolbox, 0)},
+		{"Filter-All", make(trails.Toolbox, 4), make(trails.Toolbox, 0)},
+		{
+			"From-4-To-1",
+			trails.Toolbox{
+				{}, {}, {},
+				{Actions: make([]trails.ToolAction, 1)},
+			},
+			trails.Toolbox{{Actions: make([]trails.ToolAction, 1)}},
+		},
+		{
+			"Keep-All",
+			trails.Toolbox{
+				{Actions: make([]trails.ToolAction, 1)},
+				{Actions: make([]trails.ToolAction, 1)},
+			},
+			trails.Toolbox{
+				{Actions: make([]trails.ToolAction, 1)},
+				{Actions: make([]trails.ToolAction, 1)},
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			require.Equal(t, tc.output, tc.input.Filter())
+		})
+	}
 }
 
 func TestToolRender(t *testing.T) {
-	// TODO
-	var tool trails.Tool
-	require.False(t, tool.Render())
+	for _, tc := range []struct {
+		name   string
+		input  []trails.ToolAction
+		output bool
+	}{
+		{"Nil", nil, false},
+		{"Zero", make([]trails.ToolAction, 0), false},
+		{"Has-Some", make([]trails.ToolAction, 3), true},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			actual := trails.Tool{Actions: tc.input}
+			require.Equal(t, tc.output, actual.Render())
+		})
+	}
 }
