64ce86a782f5476b4f994fddc9924af897213551
David Ketch <david@xyplanningnetwork.com>
2024-06-25 10:43:42 -0500

    http/req: moves parsing/validating functionality into ParseBody and ParseQueryParams

--- http/req/req.go
+++ http/req/req.go
@@ -7,12 +7,18 @@ import (
 	"io"
 	"net/url"
 
+	v10 "github.com/go-playground/validator/v10"
+	"github.com/gorilla/schema"
 	"github.com/xy-planning-network/trails"
 )
 
+const (
+	failedValidationTmpl = "trails/http/req: %T failed validation: %w"
+)
+
 type Parser struct {
-	queryParamDecoder queryParamDecoder
-	validator
+	queryParamDecoder *schema.Decoder
+	validator         *v10.Validate
 }
 
 func NewParser() *Parser {
@@ -29,18 +35,28 @@ func NewParser() *Parser {
 // ParseBody reads the entire r.Body and can't be read from again.
 // Use a [io.TeeReader] if r.Body needs to be reused after calling ParseBody.
 func (p *Parser) ParseBody(body io.Reader, structPtr any) error {
-	var ourFault *json.InvalidUnmarshalError
 	err := json.NewDecoder(body).Decode(structPtr)
+
+	var ourFault *json.InvalidUnmarshalError
 	if errors.As(err, &ourFault) {
-		return fmt.Errorf("trails/http/req: %w: ParseBody called with non-pointer: %s", trails.ErrBadAny, err)
+		return fmt.Errorf("trails/http/req: %w: ParseBody called with non-pointer", trails.ErrBadAny)
 	}
 
 	if err != nil {
 		return fmt.Errorf("trails/http/req: %w: failed decoding request body: %s", trails.ErrBadFormat, err)
 	}
 
-	if err := p.validate(structPtr); err != nil {
-		return fmt.Errorf("trails/http/req: %T failed validation: %w", structPtr, err)
+	err = p.validator.Struct(structPtr)
+
+	var verrs v10.ValidationErrors
+	if errors.As(err, &verrs) {
+		err = translateValidationErrors(verrs)
+		return fmt.Errorf(failedValidationTmpl, structPtr, err)
+	}
+
+	if err != nil {
+		// NOTE(dlk): return raw v10.Validator errors until use cases show up for parsing these differently.
+		return err
 	}
 
 	return nil
@@ -50,12 +66,33 @@ func (p *Parser) ParseBody(body io.Reader, structPtr any) error {
 // If successful, ParseQueryParams runs validation against the contents,
 // returning an ErrNotValid if the data fails validation rules.
 func (p *Parser) ParseQueryParams(params url.Values, structPtr any) error {
-	if err := p.queryParamDecoder.decode(structPtr, params); err != nil {
+	err := p.queryParamDecoder.Decode(structPtr, params)
+	if err != nil {
+		if err.Error() == "schema: interface must be a pointer to struct" {
+			err = fmt.Errorf("trails/http/req: %w: ParseQueryParams called with non-pointer", trails.ErrBadAny)
+
+			return err
+		}
+
+		err = translateDecoderError(err)
+		if errors.Is(err, trails.ErrNotValid) {
+			return fmt.Errorf(failedValidationTmpl, structPtr, err)
+		}
+
 		return fmt.Errorf("trails/http/req: failed decoding request query params: %w", err)
 	}
 
-	if err := p.validate(structPtr); err != nil {
-		return fmt.Errorf("trails/http/req: %T failed validation: %w", structPtr, err)
+	err = p.validator.Struct(structPtr)
+
+	var verrs v10.ValidationErrors
+	if errors.As(err, &verrs) {
+		err = translateValidationErrors(verrs)
+		return fmt.Errorf(failedValidationTmpl, structPtr, err)
+	}
+
+	if err != nil {
+		// NOTE(dlk): return raw v10.Validator errors until use cases show up for parsing these differently.
+		return err
 	}
 
 	return nil
