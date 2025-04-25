dd0628e4df29caa40f9f738f3764a088de4a2a12
David Ketch <david@xyplanningnetwork.com>
2025-04-25 14:07:21 -0500

    wip

--- postgres/service.go
+++ postgres/service.go
@@ -1,137 +1,210 @@
 package postgres
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
 	"math"
 
+	"github.com/xy-planning-network/trails"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
-// DatabaseService sets up the interface to be used at the handler/middelware level. These should be straightforward
-// calls that allow us to skip creating a procedure method for the most basic database interactions. At the procedural
-// layer, the *gorm.DB struct is available directly for more complex composition. This has the intended functionality
-// that we are not testing the database in handlers, while it is tested directly at the procedural layer.
-type DatabaseService interface {
-	CountByQuery(model any, query map[string]any) (int64, error)
-	FetchByQuery(models any, query string, params []any) error
-	FindByID(model any, ID any) error
-	FindByQuery(model any, query map[string]any) error
-	PagedByQuery(models any, query string, params []any, order string, page int, perPage int, preloads ...string) (PagedData, error)
-	PagedByQueryFromSession(models any, session *gorm.DB, page int, perPage int) (PagedData, error)
+type DB[T any] struct {
+	db *gorm.DB
 }
 
-// PagedData is returned from the PagedByQuery method. It contains paged database records and pagination metadata.
-type PagedData struct {
-	Items      any   `json:"items"`
+func Query[T any]() *DB[T] { return &DB[T]{DefaultConn.db} }
+
+func (db *DB[T]) Begin(opts ...*sql.TxOptions) *DB[T] {
+	return &DB[T]{db: db.db.Begin(opts...)}
+}
+
+func (db *DB[T]) Count() (int64, error) {
+	var count int64
+	if err := db.db.Count(&count).Error; err != nil {
+		err = fmt.Errorf("%w: %s", trails.ErrUnexpected, err)
+		return 0, err
+	}
+
+	return count, nil
+}
+
+func (db *DB[T]) Commit() error {
+	if err := db.db.Commit().Error; err != nil {
+		err = fmt.Errorf("%w: failed committing tx: %s", trails.ErrUnexpected, err)
+		return err
+	}
+
+	return nil
+}
+
+func (db *DB[T]) Create(value T) error {
+	if err := db.db.Create(value).Error; err != nil {
+		err = fmt.Errorf("%w: failed creating %T: %s", trails.ErrUnexpected, value, err)
+		return err
+	}
+
+	return nil
+}
+
+func (db *DB[T]) Delete(value T) error { return nil } // TODO
+
+func (db *DB[T]) Debug() *DB[T] { return &DB[T]{db.db.Debug()} }
+
+func (db *DB[T]) Distinct(args ...interface{}) *DB[T] {
+	return &DB[T]{db.db.Distinct(args...)}
+}
+
+func (db *DB[T]) Exists() (bool, error) { // TODo
+	var exists bool
+	err := db.db.Raw("SELECT exists(?)", nil).Error
+	if err != nil {
+		// TODO err wrap
+		return exists, err
+	}
+
+	return exists, nil
+}
+
+func (db *DB[T]) Find() ([]T, error) {
+	var dest []T
+	if err := db.db.Model(new(T)).Find(&dest).Error; err != nil {
+		return dest, fmt.Errorf("%w: %s", trails.ErrUnexpected, err)
+	}
+
+	if len(dest) == 0 {
+		return nil, fmt.Errorf("%w", trails.ErrNotFound)
+	}
+
+	return dest, nil
+}
+
+func (db *DB[T]) First() (T, error) {
+	var dest T
+	err := db.db.Model(&dest).First(&dest).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return dest, fmt.Errorf("%w", trails.ErrNotFound)
+	}
+
+	if err != nil {
+		return dest, fmt.Errorf("%w: %s", trails.ErrUnexpected, err)
+	}
+
+	return dest, nil
+}
+
+func (db *DB[T]) Group(name string) *DB[T] { return db } // TODO
+
+func (db *DB[T]) Joins(query string, args ...interface{}) *DB[T] { return db } // TODO
+
+func (db *DB[T]) Limit(limit int) *DB[T] { return db } // TODO
+
+func (db *DB[T]) Offset(offset int) *DB[T] { return db } // TODO
+
+func (db *DB[T]) Or(query interface{}, args ...interface{}) *DB[T] { return db } // TODO
+
+func (db *DB[T]) Order(value interface{}) *DB[T] {
+	return &DB[T]{db.db.Order(value)}
+}
+
+func (db *DB[T]) Paged(page, perPage int) (PagedData[T], error) {
+	var pd PagedData[T]
+	var items T
+
+	page = max(1, page)
+	perPage = max(1, perPage)
+
+	var totalRecords int64
+	err := db.db.Model(&items).Session(new(gorm.Session)).Count(&totalRecords).Error
+	if err != nil {
+		err = fmt.Errorf("%w: %s", trails.ErrUnexpected, err)
+		return pd, err
+	}
+
+	offset := (page - 1) * perPage
+	err = db.db.Model(&items).Limit(perPage).Offset(offset).Find(&items).Error
+	if err != nil {
+		err = fmt.Errorf("%w: %s", trails.ErrUnexpected, err)
+		return pd, err
+	}
+
+	pd.Items = items
+	pd.Page = page
+	pd.PerPage = perPage
+	pd.TotalItems = totalRecords
+	totalPagesFloat := float64(totalRecords) / float64(perPage)
+	pd.TotalPages = int(math.Ceil(totalPagesFloat))
+
+	return pd, nil
+}
+
+func (db *DB[T]) Preload(query string, args ...interface{}) *DB[T] {
+	return &DB[T]{db: db.db.Preload(query, args)}
+}
+
+func (db *DB[T]) Scopes(funcs ...func(*DB[T]) *DB[T]) *DB[T] { return db }
+
+func (db *DB[T]) Select(columns ...string) *DB[T] { return db }
+
+// Table specifies the name of the table to query
+// Use when T is not a struct matching a database table,
+// or, T is the table targetted in the query, but not the type returned.
+//
+// For example:
+//
+//	   ids, err := Query[[]uint].Table("users").Select("id").Find()
+//
+//	   users, err := Query[[]User].
+//		       Table("accounts").
+//		       Select("users.*").
+//		       Joins("JOIN users ON accounts.id = users.account_id").
+//		       Where("accounts.active").
+//		       Find()
+//
+// TODO(dlk):
+// - [ ] implement
+// - [ ] confirm Model usage elsewhere doesn't collide
+func (db *DB[T]) Table(name string) *DB[T] { return db }
+
+// TODO
+func (db *DB[T]) Unscoped() *DB[T] { return db }
+
+func (db *DB[T]) Where(query interface{}, args ...interface{}) *DB[T] {
+	return &DB[T]{db.db.Where(query, args...)}
+}
+
+func (db *DB[T]) Rollback() error {
+	err := db.db.Rollback().Error
+	if err != nil {
+		return fmt.Errorf("%w: failed rolling back tx: %s", trails.ErrUnexpected, err)
+	}
+
+	return nil
+}
+
+func (db *DB[T]) Update(values map[string]interface{}) ([]T, error) {
+	var dest []T
+	err := db.db.Model(&dest).Clauses(clause.Returning{}).Updates(values).Error
+	if err != nil {
+		err = fmt.Errorf("%w: %s", trails.ErrUnexpected, err)
+		return nil, err
+	}
+
+	if len(dest) == 0 {
+		return nil, fmt.Errorf("%w", trails.ErrNotFound)
+	}
+
+	return dest, nil
+}
+
+// PagedData is returned from the Paged method.
+// It contains paged database records and pagination metadata.
+type PagedData[T any] struct {
+	Items      T     `json:"items"`
 	Page       int   `json:"page"`
 	PerPage    int   `json:"perPage"`
 	TotalItems int64 `json:"totalItems"`
 	TotalPages int   `json:"totalPages"`
-}
-
-// DatabaseServiceImpl satisfies the above DatabaseService interface.
-type DatabaseServiceImpl struct {
-	DB *gorm.DB
-}
-
-// NewService hydrates the gorm database for the implementation struct methods.
-func NewService(DB *gorm.DB) *DatabaseServiceImpl {
-	return &DatabaseServiceImpl{DB: DB}
-}
-
-// CountByQuery recives a database model and query and fetches a count for the given params.
-func (service *DatabaseServiceImpl) CountByQuery(model any, query map[string]any) (int64, error) {
-	count := int64(0)
-	return count, service.DB.Model(model).Where(query).Count(&count).Error
-}
-
-// FetchByQuery receives a slice of database models as a pointer and fetches all records matching the query.
-func (service *DatabaseServiceImpl) FetchByQuery(models any, query string, params []any) error {
-	return service.DB.Where(query, params...).Find(models).Error
-}
-
-// FindByID receives a database model as a pointer and fetches it using the primary ID.
-func (service *DatabaseServiceImpl) FindByID(model any, ID any) error {
-	return service.DB.First(model, ID).Error
-}
-
-// FindByQuery receives a database model as a pointer and fetches it using the given query.
-func (service *DatabaseServiceImpl) FindByQuery(model any, query map[string]any) error {
-	return service.DB.Where(query).First(model).Error
-}
-
-// Insert receives a database model and inserts it into the database.
-func (service *DatabaseServiceImpl) Insert(model any) error {
-	return service.DB.Create(model).Error
-}
-
-// PagedByQuery receives a slice of database models and paging information to build a paged database query.
-func (service *DatabaseServiceImpl) PagedByQuery(models any, query string, params []any, order string, page int, perPage int, preloads ...string) (PagedData, error) {
-	pd := PagedData{}
-
-	// Make sure page/perPage are sane
-	if page < 1 {
-		page = 1
-	}
-	if perPage < 1 || perPage > 100 {
-		perPage = 10
-	}
-
-	// Conduct unlimited count query to calculate totals
-	var totalRecords int64
-	if err := service.DB.Where(query, params...).Model(models).Count(&totalRecords).Error; err != nil {
-		return pd, err
-	}
-
-	// Calculate offset and conduct limited query
-	offset := (page - 1) * perPage
-	session := service.DB
-	for _, preload := range preloads {
-		session = session.Preload(preload)
-	}
-	if err := session.Where(query, params...).Order(order).Limit(perPage).Offset(offset).Find(models).Error; err != nil {
-		return pd, err
-	}
-
-	pd.Items = models
-	pd.Page = page
-	pd.PerPage = perPage
-	pd.TotalItems = totalRecords
-	totalPagesFloat := float64(totalRecords) / float64(perPage)
-	pd.TotalPages = int(math.Ceil(totalPagesFloat))
-
-	return pd, nil
-}
-
-// PagedByQueryFromSession receives a slice of database models and paging information to build a paged database query.
-func (service *DatabaseServiceImpl) PagedByQueryFromSession(models any, session *gorm.DB, page int, perPage int) (PagedData, error) {
-	pd := PagedData{}
-
-	// Make sure page/perPage are sane
-	if page < 1 {
-		page = 1
-	}
-	if perPage < 1 || perPage > 100 {
-		perPage = 10
-	}
-
-	// Conduct unlimited count query to calculate totals
-	var totalRecords int64
-	if err := session.Model(models).Count(&totalRecords).Error; err != nil {
-		return pd, err
-	}
-
-	// Calculate offset and conduct limited query
-	offset := (page - 1) * perPage
-	if err := session.Session(&gorm.Session{QueryFields: true}).Limit(perPage).Offset(offset).Find(models).Error; err != nil {
-		return pd, err
-	}
-
-	pd.Items = models
-	pd.Page = page
-	pd.PerPage = perPage
-	pd.TotalItems = totalRecords
-	totalPagesFloat := float64(totalRecords) / float64(perPage)
-	pd.TotalPages = int(math.Ceil(totalPagesFloat))
-
-	return pd, nil
 }
